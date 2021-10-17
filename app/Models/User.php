fe85c14bb7948b7ac4e8ba9a9521b373b7f8b9c2
abir bikal <tipu.dany@gmail.com>
2021-10-18 00:36:23 +0600

    User model eidt

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,9 +47,4 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
-    /*public function comments()
-    {
-        return $this->hasMany(Comment::class);
-    }*/
-
 }
